e0021638f9cdbf61fbe04854d746671b2dcd90f9
MillerC <millerpoli9@gmail.com>
2018-02-26 17:30:07 -0500

    first commit

---
+++ 1numeros.py
@@ -0,0 +1,58 @@
+def numeros(a,b):
+	# print ("Los argumentos de la función son: 12 y 5")
+	suma = 12+5	
+	print ("Suma 		12+5: "+str(suma))
+	resta = 12-5
+	print ("Resta 		12-5: "+str(resta))
+	multiplicacion = 12*5
+	print ("Multiplicación 	12*5: "+str(multiplicacion))
+	division = 12/5
+	print ("División 	12/5: "+str(division))
+	modulo = 12/5
+	print ("Módulo:		12%5: "+str(modulo))
+	potencia = 3**2
+	print ("Potencia 3^2: "+str(potencia))
+	op = 3-2+4*10
+	print ("Python interpretará automáticamente las prioridades de los operadores: --> 3-2+4*10 ")
+	print ("Operación:	3-2+4*10: "+str(op))
+	print ("Variables:")
+	print ("Se pueden usar como si se tratase de un valor literal, incluso operarlas entre otras variables y volver a asignarles un valor en cualquier momento.")	
+	print ("n=3")
+	n=3
+	# n=n+3
+	print ("n+5:	"+str(n+5))
+	print ("n*2:	"+str(n*2))
+	print ("n*n:	"+str(n*n))
+	print ("n%2:	"+str(n%2))
+	m=10	
+	print ("m=10")
+	print ("n+m: "+str(n+m))
+	print ("n*m+15:	"+str(n*m+15))
+	print ("Reasignación:")
+	n=10
+	m=15	
+	print ("n=10, m=15")
+	print ("n+m:	"+str(n+m))
+	n=m+10	
+	print ("n=m+10:	"+str(n))
+	n=n+25
+	print ("n=n+25: "+str(n))
+	nota_1=2
+	nota_2=5
+	nota_media=(nota_1 + nota_2)/2
+	print ("nota_1=2, nota_2=5, el promedio es: "+str(nota_media))
+
+
+numeros(12,5)	
+
+
+
+
+
+
+
+
+
+
+
+
