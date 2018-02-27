8f44c2e7876c42eeef057c578f0cd6b9e35aca69
MillerC <millerpoli9@gmail.com>
2018-02-27 11:34:02 -0500

    archivo completado

--- 4lectura_por_teclado.py
+++ 4lectura_por_teclado.py
@@ -20,10 +20,43 @@ def lectura():
 	print ("Lo que se ingresa por teclado siempre son cadenas = Strings:")
 	print ("# Una cadena y un número no se pueden operar ==> valor + 100 ")
 	print ("Lanza error porque no se puede sumar un string con un entero")
+	# Convierto el String "valor" en un entero:
+	valor = int(valor)
+	# Imprimo el entero y ahora si se puede operar con otros enteros:	
 	print ("---")
 	print ("")
+	val_entero = input("Introduce un número entero: ")
+	print ("El entero ingresado es: "+str(val_entero))
+	print ("---")
+	print ("Para convertir a entero ==> Cast con int(), de cadena a entero:")
+	# La función int() de entero, devuelve un número entero a partir de una cadena
+	print ("valor = "+str(valor))	
+	valor = int(valor)
+	print ("valor + 15 = "+str(valor+15))
+	print ("---")
+	print ("Para convertir a float ==> Cast con float(), de cadena a flotante:")
+	# La función float() de flotante, devuelve un número flotante a partir de una cadena
+	valor = float(valor) 
+	print ("El 'valor' que tenía guardado, pero ahora en tipo float es: "+str(valor))
+	print ("10 + valor = "+str(10+valor))
+	print ("---")
+	print ("Lectura por teclado + cast a float, se haría así:")
+	print ('value = float( input("Introduce un número decimal o entero: ") )')
+	value = float( input("Introduce un número decimal o entero: ") )
+	print ("El numero ingresado es convertido a tipo float: "+str(value))
+	print ("---")
+
 
 
 
 
 lectura()
+
+
+
+
+
+
+
+
+
