7ff3022a6c9b195fdee02281daac0f6d44bd73b6
MillerC <millerpoli9@gmail.com>
2018-02-28 08:53:28 -0500

    Part4

---
+++ 4Operadores_y_expresiones/19tipo_logico.py
@@ -0,0 +1,22 @@
+#	Tipo Lógico: 
+#	Mínima expresión racional
+#	Representada por dos valores, True y False
+
+def tipo_logico():
+	print ("Tipo Lógico 	==> 1+1==3")#devuelve False
+	print (1+1==3)
+	print ("Tipo Lógico 	==> 1+1==2")#devuelve True
+	print (1+1==2)
+
+tipo_logico()
+
+
+
+
+
+
+
+
+
+
+
---
+++ 4Operadores_y_expresiones/20operadores_relacionales.py
@@ -0,0 +1,39 @@
+#	Los operadores relacionales
+#	Sirven para comparar dos valores, dependiendo del #	resultado de la comparación pueden devolver:
+#	Verdadero (True), si es cierta
+#	Falso (False), si no es cierta
+
+
+def op_relacional():
+	print ("Igual que 	==> 3==2")
+	print (3 == 2) #devuelve False
+	print ("Distinto de	==> 3!=2") 
+	print (3 != 2) #devuelve True
+	print ("Mayor que	==> 3>2") 
+	print (3 > 2)
+	print ("Menor que	==> 3<2")
+	print (3 < 2)
+	print ("Mayor o igual 	==> 3>=2")
+	print (3>=2)
+	print ("Menor o igual 	==> 3<=2")
+	print (3<=2)
+	print ("Menor o igual 	==> 1<=2")
+	print (1<=2)
+	print ("---")
+	print ("Variables: a=10, b=5:")
+	a=10
+	b=5
+	print ("a es menor o igual que b?")
+	print (a<=b)
+	print ("a es mayor o igual que b?")
+	print (a>=b)
+	
+
+op_relacional()
+
+
+
+
+
+
+
