055493ab4ca00a0a2a867d277faf55cc86be9dfb
MillerC <millerpoli9@gmail.com>
2018-03-01 08:48:51 -0500

    Ejercicio condicionales 2

---
+++ 4Operadores_y_expresiones/23_b_exercise.py
@@ -0,0 +1,22 @@
+"""
+2) Utilizando operadores lógicos, determina si una cadena de texto introducida por el usuario tiene una longitud mayor o igual que 3 y a su vez es menor que 10 (es suficiene con mostrar True o False):
+"""
+
+def long_cad():
+	print ("---")
+	print ("Please enter the string to compute the length:")
+	str_1=input()
+	print ("The string length is greater or equal than 3 and less than 10 ?")
+	print ( (len(str_1)>=3) and (len(str_1)<10) )
+	print ("---")
+
+
+
+
+
+long_cad()
+
+
+
+
+
