a9d2746442461f73f7d0ca3ca55c9fd2f13258f1
MillerC <millerpoli9@gmail.com>
2018-02-28 19:17:38 -0500

    Not, And, Or

---
+++ 4Operadores_y_expresiones/21operadores_logicos.py
@@ -0,0 +1,68 @@
+"""
+Operadores lógicos:
+Encontramos 3 básicos:
+    Not
+    And
+    Or
+"""
+
+
+def opera_logico():
+	print ("---")	
+	print ("Not ==> Negación Lógica:")
+	print ("not True ?")
+	print (not True)
+	print ("not True == False ?")
+	print (not True == False)
+	print ("---")
+	print ("And ==> Conjunción Lógica:")
+	print ("True and True ?")
+	print (True and True)
+	print ("True and False ?")
+	print (True and False)
+	print ("False and True ?")
+	print (False and True)
+	print ("False and False ?")
+	print (False and False)
+	print ("---")
+	print ("Variables: a=13, b=7 ")
+	a=13
+	b=7
+	print ("a>10 and a<20 ?")
+	print (a>10 and a<20)
+	print ("b>10 and b<20 ?")
+	print (b>10 and b<20)
+	print ("---")
+	c="Hola Mundo"	
+	print ("c='Hola Mundo'")
+	print ("len(c) >= 5 and c[0]=='H' ?")
+	print (len(c) >= 5 and c[0]=='H')
+	print ("---")
+	print ("Or ==> Si tan solo una sentencia es verdadera, retorna True:")
+	print ("True or True ?")
+	print (True or True)
+	print ("True or False ?")
+	print (True or False)
+	print ("False or True ?")
+	print (False or True)
+	print ("False or False ?")
+	print (False or False)
+	print ("---")
+
+
+
+
+
+
+
+opera_logico()
+
+
+
+
+
+
+
+
+
+
