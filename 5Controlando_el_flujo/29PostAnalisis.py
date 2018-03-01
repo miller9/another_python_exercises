a051372c600d629b72ef02403231439422304957
MillerC <millerpoli9@gmail.com>
2018-03-01 14:38:45 -0500

    par_impar

---
+++ 5Controlando_el_flujo/29PostAnalisis.py
@@ -0,0 +1,13 @@
+def par_impar():
+	n = 0
+	while n < 10:
+		if (n % 2) == 0:
+			print(n,'es un número par')
+			n=n+1	
+		else:
+			print(n,'es un número impar') 
+			n=n+1	
+	print ("---")
+
+
+par_impar()
