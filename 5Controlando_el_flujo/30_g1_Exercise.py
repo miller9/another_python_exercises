f7c36b0a71fe79c5a55048e1cea87249d1865ab6
MillerC <millerpoli9@gmail.com>
2018-03-02 17:58:29 -0500

    comparing 2 lists, creating a new one

--- 5Controlando_el_flujo/30_g1_Exercise.py
+++ 5Controlando_el_flujo/30_g1_Exercise.py
@@ -1,21 +1,19 @@
 def comparing_lists():
 	print ("""
-7) Dadas dos listas, debes generar una tercera con todos los elementos que se repitan en ellas, pero no debe repetise ningún elemento en la nueva lista:	
+	7) Dadas dos listas, debes generar una tercera con todos los elementos que se repitan en ellas, 
+	pero no debe repetise ningún elemento en la nueva lista:	
 	""")
 	print ("---")
 	a=(list(range(0,31,3)))
 	print ("First list is:	", a)
 	b=(list(range(0,31,2)))
 	print ("Second list is:	", b)
-	i=0
-	cont=0
-#	c=[1]*len(a)
-	c=" "	#Its working, but its a string, it must be a list....
-
+	
+	c=" "			#Its working, but its a string, it must be a list....
+	
 	for index,value in enumerate(a):
 		if (a[index] in b):
 			c += str(value)+","
-			i+=1
 
 	print ("New list is: 	",c)
 	print ("")
