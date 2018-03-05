e9bb1c1daa7cdfc12f70f5999d5c3ed76ffe6bc7
MillerC <millerpoli9@gmail.com>
2018-03-05 18:39:31 -0500

    Ejecutar scripts

---
+++ 7Entradas_Salidas_de_datos/40Scripts.py
@@ -0,0 +1,4 @@
+import sys			#Importar la libreria del sistema
+print ("hello, how are you?")
+print (sys.argv)	#variable, propiedad del sisteam ==> argv
+# --> en terminal, ejecutar: python 40Scripts.py "una cadena de texto" 5
