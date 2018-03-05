4af597f1276f47c5efe70051ed303d2f8f3c840d
MillerC <millerpoli9@gmail.com>
2018-03-05 13:25:29 -0500

    Pilas, append(), pop()  Colas, deque(), deque([]), popleft()

--- 6Colecciones_de_datos/36Pilas_y_Colas_ConListas.py
+++ 6Colecciones_de_datos/36Pilas_y_Colas_ConListas.py
@@ -46,34 +46,66 @@ def pilas_colas():
 	pila.pop()
 	print ("pila.pop()")
 	print ("pila			:	", pila)
+	print ()
+	print ()
+	print ("""	Las colas
 
+	Son colecciones de elementos ordenados que únicamente permiten dos acciones:
 
+	Añadir un elemento a la cola
+	Sacar un elemento de la cola
 
+	La peculiaridad es que el primer elemento en entrar es el primero en salir. 
+	En inglés se conocen como estructuras FIFO (First In First Out).
+	Debemos importar la colección deque manualmente para crear una cola:
 
+	from collections import deque
+		
+	""")
+	from collections import deque
+	print ("cola = deque()")
+	cola = deque()
+	print ("cola			:	", cola)
+	print ()
+	print (" Podemos añadir elemento directamente pasando una lista a la cola al crearla:")	
+	print ()
+	print ("cola = deque(['Hector','Juan','Miguel'])")
+	cola = deque(['Hector','Juan','Miguel'])
+	print ("cola			:	", cola)
+	print ()
+	print (" Y también utilizando el método .append():")
+	print ("cola.append('Maria')")
+	cola.append('Maria')
+	print ("cola			:	", cola)
+	print ()
+	print ("cola.append('Arnaldo')")
+	cola.append('Arnaldo')
+	print ("cola			:	", cola)
+	print ()
+	print ("""	popleft() en lugar de pop()
 
+	A la hora de sacar los elementos utilizaremos el método popleft() para extraerlos por la parte
+	izquierda (el principio de la cola).
+	Al igual que antes, debemos asegurarnos de almacenar los elementos al sacarlos o los perderemos.
 
-
-
+	
+	""")
+	print ("cola		  :", cola)
+	print ()
+	print ("cola.popleft()	  : Eliminar primer elemento")
+	cola.popleft()
+	print ("cola		  :", cola)
+	print ()
+	x = cola.popleft()
+	print ("x = cola.popleft()")
+	print ("x		  :", x)
+	print ()
+	print ("cola		  :", cola)
+	print ()
 	print ("---")
+	print ()
 
 pilas_colas()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
