15e8cdea86e8953695f537764bd3ac75ddbe2336
MillerC <millerpoli9@gmail.com>
2018-03-06 09:09:58 -0500

    .format() para cadenas y numeros

---
+++ 7Entradas_Salidas_de_datos/42_a_exercise.py
@@ -0,0 +1,45 @@
+def a42():
+	print ("""
+		1) Formatea los siguientes valores para mostrar el resultado indicado:
+
+		"Hola Mundo" → Alineado a la derecha en 20 caracteres
+		"Hola Mundo" → Truncamiento en el cuarto carácter (índice 3)
+		"Hola Mundo" → Alineamiento al centro en 20 caracteres con truncamiento en el segundo carácter (índice 1)
+		150 → Formateo a 5 números enteros rellenados con ceros
+		7887 → Formateo a 7 números enteros rellenados con espacios
+		20.02 → Formateo a 3 números enteros y 3 números decimales
+
+		""")
+	resultado = "Hola Mundo"
+	print ("{:^20}".format(resultado))
+	print ("{:.3}".format(resultado))
+	print ("{:^20.1}".format(resultado))
+	num1 = 150
+	num2 = 7887
+	num3 = 20.02
+	print("\n{:05d}".format(num1))
+	print("{:7d}".format(num2))
+	print("{:07.3f}".format(num3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a42()
