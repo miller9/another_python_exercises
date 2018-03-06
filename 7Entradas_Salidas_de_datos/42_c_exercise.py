e6b24328f41b59d3df5dfd8466878c14ac13460d
MillerC <millerpoli9@gmail.com>
2018-03-06 18:30:45 -0500

    descomposición unidades, decenas, centenas

--- 7Entradas_Salidas_de_datos/42_c_exercise.py
+++ 7Entradas_Salidas_de_datos/42_c_exercise.py
@@ -27,7 +27,7 @@ import sys
 if len(sys.argv) == 2:
     numero = int(sys.argv[1])
     if numero < 0 or numero > 9999:
-        print("Error - Número es incorrecto")
+        print("\nError - Número es incorrecto")
         print("Ejemplo: descomposicion.py [0-9999]")
     else:
         # Aqui va la lógica
