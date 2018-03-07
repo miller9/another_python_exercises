6d22296fa0f7a68cfb9abe385ccd48a135f46528
MillerC <millerpoli9@gmail.com>
2018-03-07 18:49:44 -0500

    try, except TypeError

---
+++ 9Manejo_de_excepciones/59_d_exercise.py
@@ -0,0 +1,18 @@
+print ("""
+4) Localiza el error en el siguiente bloque de código. 
+Crea una excepción para evitar que el programa se bloquee
+y además explica en un mensaje al usuario la causa y/o solución:
+
+resultado = 15 + "20"
+--> El programa se bloquea porque no se puede sumar un entero con una cadena
+--> Error que lanza = TypeError: unsupported operand type(s) for +: 'int' and 'str'
+	""")
+
+try:
+	resultado = 15 + "20"
+except TypeError:
+	print ("No se puede sumar un entero con una cadena")
+	print ("Transforma la cadena a numero --> int(20)")
+
+print ()
+print ()
