5c96ff8196a78d60b71cded540c4c27620b7b913
MillerC <millerpoli9@gmail.com>
2018-03-07 14:30:51 -0500

    area_rectangulo()

---
+++ 8Manejo_de_datos_y_optimizacion/53_a_exercise.py
@@ -0,0 +1,14 @@
+print ("""
+1) Realiza una función llamada area_rectangulo() que devuelva el área del rectangulo a partir de una base y una altura.
+Calcula el área de un rectángulo de 15 de base y 10 de altura.
+	""")
+
+def area_rectangulo(base,altura):
+	return (base*altura)
+
+res=area_rectangulo(15,10)
+print ("El área de rectangulo de base 15 y altura 10 es:",res,"cm2")
+print ()
+
+
+
