cc4aabf2db42617c5423f2372afc7a64b2885a90
MillerC <millerpoli9@gmail.com>
2018-03-07 14:32:46 -0500

    intermedio()

---
+++ 8Manejo_de_datos_y_optimizacion/53_d_exercise.py
@@ -0,0 +1,15 @@
+print ("""
+4) Realiza una función llamada intermedio() que a partir de dos números, devuelva su punto intermedio:
+
+Nota: El número intermedio de dos números corresponde a la suma de los dos números dividida entre 2
+
+Comprueba el punto intermedio entre -12 y 24
+
+	""")
+
+def intermedio(a,b):
+	return (a+b)/2
+
+
+res=intermedio(-12,24)
+print ("El punto intermedio entre -12 y 24 es:",res)
