79afba60a6dae6e73914f95ba7f2a132f7c02349
MillerC <millerpoli9@gmail.com>
2018-03-12 18:29:44 -0500

    try, except KeyError

--- 9Manejo_de_excepciones/59_c_exercise.py
+++ 9Manejo_de_excepciones/59_c_exercise.py
@@ -1,12 +1,12 @@
 print ("""
-3) Localiza el error en el siguiente bloque de código. 
-Crea una excepción para evitar que el programa se bloquee
-y además explica en un mensaje al usuario la causa y/o solución:
+	3) Localiza el error en el siguiente bloque de código. 
+	Crea una excepción para evitar que el programa se bloquee
+	y además explica en un mensaje al usuario la causa y/o solución:
 
-colores = { 'rojo':'red', 'verde':'green', 'negro':'black' } 
-colores['blanco']
---> El programa no tiene una clave 'blanco', por ello se bloquea
---> Error que lanza = KeyError: 'blanco'
+	colores = { 'rojo':'red', 'verde':'green', 'negro':'black' } 
+	colores['blanco']
+	--> El programa no tiene una clave 'blanco', por ello se bloquea
+	--> Error que lanza = KeyError: 'blanco'
 	""")
 
 colores = { 'rojo':'red', 'verde':'green', 'negro':'black' } 
@@ -20,7 +20,7 @@ except KeyError:
 		print ("Claves válidas: 'rojo', 'verde', 'negro'.")
 
 """
-try:
+try:										# otra forma
 	colores['blanco']
 except KeyError:
 	print ("La clave ingresada no está en el diccionario.")
