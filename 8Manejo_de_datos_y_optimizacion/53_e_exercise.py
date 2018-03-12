ad5c0ededf787ee626f9c53c84f2d49d11d055bb
MillerC <millerpoli9@gmail.com>
2018-03-12 18:24:06 -0500

    recortar(numero,minimo,maximo)

--- 8Manejo_de_datos_y_optimizacion/53_e_exercise.py
+++ 8Manejo_de_datos_y_optimizacion/53_e_exercise.py
@@ -1,16 +1,15 @@
 print ("""
-5) Realiza una función llamada recortar() que reciba tres parámetros. 
-El primero es el número a recortar, 
-el segundo es el límite inferior y 
-el tercero el límite superior. 
+	5) Realiza una función llamada recortar() que reciba tres parámetros. 
+	El primero es el número a recortar, 
+	el segundo es el límite inferior y 
+	el tercero el límite superior. 
 
-La función tendrá que cumplir lo siguiente:
-    Devolver el límite inferior si el número es menor que éste
-    Devolver el límite superior si el número es mayor que éste.
-    Devolver el número sin cambios si no se supera ningún límite.
+	La función tendrá que cumplir lo siguiente:
+	    Devolver el límite inferior si el número es menor que éste
+	    Devolver el límite superior si el número es mayor que éste.
+	    Devolver el número sin cambios si no se supera ningún límite.
 
-Comprueba el resultado de recortar 15 entre los límites 0 y 10
-
+	Comprueba el resultado de recortar 15 entre los límites 0 y 10
 	""")
 
 def recortar(numero,minimo,maximo):
