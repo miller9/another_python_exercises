443d9426c8a0c35308846fc748d8c9f1602d57fe
MillerC <millerpoli9@gmail.com>
2018-03-12 18:31:54 -0500

    agregar_una_vez(lista, elem) --> try,raise,except

--- 9Manejo_de_excepciones/59_e_exercise.py
+++ 9Manejo_de_excepciones/59_e_exercise.py
@@ -1,16 +1,14 @@
 print ("""
-5) Realiza una función llamada agregar_una_vez() que reciba una lista y un elemento. 
-La función debe añadir el elemento al final de la lista con la condición 
-de no repetir ningún elemento. Además si este elemento ya se encuentra en la lista 
-se debe invocar un error de tipo ValueError que debes capturar y mostrar este mensaje en su lugar:
+	5) Realiza una función llamada agregar_una_vez() que reciba una lista y un elemento. 
+	La función debe añadir el elemento al final de la lista con la condición 
+	de no repetir ningún elemento. Además si este elemento ya se encuentra en la lista 
+	se debe invocar un error de tipo ValueError que debes capturar y mostrar este mensaje en su lugar:
 
-  Error: Imposible añadir elementos duplicados => [elemento].
+	  Error: Imposible añadir elementos duplicados => [elemento].
 
-Prueba de agregar los elementos 10, -2, "Hola" a la lista de elementos con
-la función una vez la has creado y luego muestra su contenido.
-Nota: Puedes utilizar la sintaxis: elemento in lista
-
-
+	Prueba de agregar los elementos 10, -2, "Hola" a la lista de elementos con
+	la función una vez la has creado y luego muestra su contenido.
+	Nota: Puedes utilizar la sintaxis: elemento in lista
 	""")
 
 elementos = [1,5,-2]
